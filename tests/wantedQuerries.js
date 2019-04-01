df456d2684eef983fd12af0464fc6760d9708fce
Justin Ford <jmford7@gmai.com>
2019-04-01 12:06:20 -0600

    part 3

--- tests/wantedQuerries.js
+++ tests/wantedQuerries.js
@@ -17,11 +17,12 @@ module.exports = {
     },
     'Submit a valid query Enter (Happy Path)': browser => {
         wantedPOM
+        .startCheck('Version 1.0')
         .click('@clickHam')
         .waitForElementPresent('@clickEnter',5000)
         .pause(5000)
         .click('@clickEnter')
-        .setEnterFields('1234567890','ABC.','OAI123456','Harry Dresden','M','B','607','225','Brown','Arson','02312018') // want to modify this into an object to call
+        .setEnterFields('1234567890','ABC.','OAI123456','Harry Dresden','M','B','607','225','Brown','Arson','02312018') // want to modify this into an variety of objects to call (add max, min, mid range values)
         .pause(5000)
         .click('@saveBtn')
         wantedPOM.expect.element('@validHdr').text.to.equal("Valid")
@@ -34,15 +35,15 @@ module.exports = {
         .waitForElementPresent('@clickEnter',5000)
         .pause(5000)
         .click('@clickEnter')
-        .setEnterFields('102345678901233548977451125588784','4444','OAI12345$','Harry Dresden','M','B','6d','2lb','Brown$S','Arson','10312010')
+        .setEnterFields('102345678901233548977451125588784','4444','OAI12345$','Harry Dresden','M','B','6d','2lb','Brown$S','Arson','10312010') // want to modify this into a variety of invalid objects (blank, more than max, chars not allowed, add appropriate expect verifications and cycle through)
         .pause(5000)
         .click('@saveBtn')
         .expect.element('@validHdr').text.to.equal("Errors Received:")
     },
-    'Check for error messages Enter': browser => {
-        wantedPOM
-    },
-    'Accepts Max values Enter': browser => {
-        wantedPOM
-    }
+    // 'Check for error messages Enter': browser => {
+    //     wantedPOM
+    // },
+    // 'Accepts Max values Enter': browser => {
+    //     wantedPOM
+    // }
 }
